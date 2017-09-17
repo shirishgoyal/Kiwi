bdc070e0cc89831c1c40dacd91ed00c6d17e298c
eliott PARIS <eliottparis@gmail.com>
2017-09-17 17:10:00 +0200

    Separate model::Factory implementation

--- Modules/KiwiModel/KiwiModel_Factory.cpp
+++ Modules/KiwiModel/KiwiModel_Factory.cpp
@@ -23,182 +23,254 @@
 
 #include "KiwiModel_Factory.h"
 
-namespace kiwi
-{
-    namespace model
+namespace kiwi { namespace model {
+    // ================================================================================ //
+    //                                      FACTORY                                     //
+    // ================================================================================ //
+    
+    std::unique_ptr<model::Object> Factory::create(std::vector<Atom> const& atoms)
     {
-        // ================================================================================ //
-        //                                      FACTORY                                     //
-        // ================================================================================ //
+        const auto typed_name = !atoms.empty() ? atoms[0].getString() : "";
         
-        std::unique_ptr<model::Object> Factory::create(std::vector<Atom> const& atoms)
+        if(typed_name.empty())
         {
-            const auto typed_name = !atoms.empty() ? atoms[0].getString() : "";
+            return createNewBox();
+        }
+        
+        const auto createErrorBox = [&typed_name, &atoms](std::string const& error_message) {
             
-            if(typed_name.empty())
-            {
-                return createNewBox();
-            }
+            static auto errorbox_class_name = "errorbox";
+            auto object = std::make_unique<ErrorBox>(typed_name, atoms);
             
-            const auto createErrorBox = [&typed_name, &atoms](std::string const& error_message) {
-                
-                static auto errorbox_class_name = "errorbox";
-                auto object = std::make_unique<ErrorBox>(typed_name, atoms);
-                
-                object->setError(error_message);
-                initObjectInfos(*object, errorbox_class_name, typed_name, AtomHelper::toString(atoms));
-                
-                return object;
-                
-            };
-            
-            auto const* object_class = getClassByName(typed_name);
-            
-            if(object_class == nullptr)
-            {
-                return createErrorBox("object \"" + typed_name + "\" not found");
-            }
-            
-            std::unique_ptr<model::Object> object = nullptr;
-            
-            try
-            {
-                std::vector<Atom> args(atoms.empty() ? atoms.begin() : atoms.begin() + 1, atoms.end());
-                object = object_class->create(args);
-            }
-            catch(std::runtime_error & e)
-            {
-                return createErrorBox(e.what());
-            }
-            
-            initObjectInfos(*object, object_class->getClassName(), typed_name, AtomHelper::toString(atoms));
+            object->setError(error_message);
+            initObjectInfos(*object, errorbox_class_name, typed_name, AtomHelper::toString(atoms));
             
             return object;
+            
+        };
+        
+        auto const* object_class = getClassByName(typed_name);
+        
+        if(object_class == nullptr)
+        {
+            return createErrorBox("object \"" + typed_name + "\" not found");
         }
         
-        std::unique_ptr<model::Object> Factory::createNewBox()
+        std::unique_ptr<model::Object> object = nullptr;
+        
+        try
         {
-            static auto newbox_class_name = "newbox";
-            static auto* newbox_class = getClassByName(newbox_class_name);
-            
-            assert(newbox_class);
-            
-            static std::vector<Atom> args = {};
-            auto object = newbox_class->create(args);
-            initObjectInfos(*object, newbox_class_name, "", "");
-            
-            return object;
+            std::vector<Atom> args(atoms.empty() ? atoms.begin() : atoms.begin() + 1, atoms.end());
+            object = object_class->create(args);
+        }
+        catch(std::runtime_error & e)
+        {
+            return createErrorBox(e.what());
         }
         
-        void Factory::initObjectInfos(model::Object& object,
-                                      std::string const& class_name,
-                                      std::string const& typed_name,
-                                      std::string const& text)
-        {
-            object.m_class_name = class_name;
-            object.m_typed_name = typed_name;
-            object.m_text = text;
-        }
+        initObjectInfos(*object, object_class->getClassName(), typed_name, AtomHelper::toString(atoms));
         
-        std::unique_ptr<model::Object> Factory::create(std::string const& name, flip::Mold const& mold)
-        {
-            auto const* class_ptr = getClassByName(name);
-            if(class_ptr != nullptr)
-            {
-                return class_ptr->moldCast(mold);
-            }
-            else
-            {
-                throw std::runtime_error("Factory can't create object " + name);
-            }
-        }
+        return object;
+    }
+    
+    std::unique_ptr<model::Object> Factory::createNewBox()
+    {
+        static auto newbox_class_name = "newbox";
+        static auto* newbox_class = getClassByName(newbox_class_name);
         
-        void Factory::copy(model::Object const& object, flip::Mold& mold)
-        {
-            const auto name = object.getClassName();
-            auto const* class_ptr = getClassByName(name);
-            if(class_ptr != nullptr)
-            {
-                class_ptr->moldMake(object, mold);
-            }
-            else
-            {
-                throw std::runtime_error("can't copy object " + name);
-            }
-        }
+        assert(newbox_class);
         
-        bool Factory::has(std::string const& name)
-        {
-            const auto& object_classes = getClasses();
-            for(const auto& object_class : object_classes)
-            {
-                if(object_class->getClassName() == name || object_class->hasAlias(name))
-                    return true;
-            }
-            
-            return false;
-        }
+        static std::vector<Atom> args = {};
+        auto object = newbox_class->create(args);
+        initObjectInfos(*object, newbox_class_name, "", "");
         
-        Factory::ObjectClassBase* Factory::getClassByName(std::string const& name,
-                                                          const bool ignore_aliases)
+        return object;
+    }
+    
+    void Factory::initObjectInfos(model::Object& object,
+                                  std::string const& class_name,
+                                  std::string const& typed_name,
+                                  std::string const& text)
+    {
+        object.m_class_name = class_name;
+        object.m_typed_name = typed_name;
+        object.m_text = text;
+    }
+    
+    std::unique_ptr<model::Object> Factory::create(std::string const& name, flip::Mold const& mold)
+    {
+        auto const* class_ptr = getClassByName(name);
+        if(class_ptr != nullptr)
         {
-            const auto& object_classes = getClasses();
-            for(const auto& object_class : object_classes)
-            {
-                if(object_class->getClassName() == name || (!ignore_aliases && object_class->hasAlias(name)))
-                    return object_class.get();
-            }
-            
-            return nullptr;
+            return class_ptr->moldCast(mold);
         }
-        
-        std::vector<std::string> Factory::getNames(const bool ignore_aliases, const bool ignore_internals)
+        else
         {
-            auto const& object_classes = getClasses();
-            std::vector<std::string> names;
-            names.reserve(object_classes.size());
-            
-            for(const auto& object_class : object_classes)
-            {
-                if(!object_class->isInternal() || !ignore_internals)
-                {
-                    names.emplace_back(object_class->getClassName());
-                    
-                    if(!ignore_aliases && object_class->hasAlias())
-                    {
-                        const auto aliases = object_class->getAliases();
-                        names.insert(names.end(), aliases.begin(), aliases.end());
-                    }
-                }
-            }
-            
-            return names;
-        }
-        
-        std::string Factory::sanitizeName(std::string const& name)
-        {
-            std::string model_name = "cicm.kiwi.object.";
-            
-            static const std::string valid_chars("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890_.");
-            
-            for(auto const& c : name)
-            {
-                if(c == '~')
-                {
-                    model_name.append("_tilde");
-                    continue;
-                }
-                
-                model_name += (valid_chars.find(c) != std::string::npos) ? c : '_';
-            }
-            
-            return model_name;
-        }
-        
-        auto Factory::getClasses() -> object_classes_t&
-        {
-            static object_classes_t static_object_classes;
-            return static_object_classes;
+            throw std::runtime_error("Factory can't create object " + name);
         }
     }
-}
+    
+    void Factory::copy(model::Object const& object, flip::Mold& mold)
+    {
+        const auto name = object.getClassName();
+        auto const* class_ptr = getClassByName(name);
+        if(class_ptr != nullptr)
+        {
+            class_ptr->moldMake(object, mold);
+        }
+        else
+        {
+            throw std::runtime_error("can't copy object " + name);
+        }
+    }
+    
+    bool Factory::has(std::string const& name)
+    {
+        const auto& object_classes = getClasses();
+        for(const auto& object_class : object_classes)
+        {
+            if(object_class->getClassName() == name || object_class->hasAlias(name))
+                return true;
+        }
+        
+        return false;
+    }
+    
+    Factory::ObjectClassBase* Factory::getClassByName(std::string const& name,
+                                                      const bool ignore_aliases)
+    {
+        const auto& object_classes = getClasses();
+        for(const auto& object_class : object_classes)
+        {
+            if(object_class->getClassName() == name || (!ignore_aliases && object_class->hasAlias(name)))
+                return object_class.get();
+        }
+        
+        return nullptr;
+    }
+    
+    std::vector<std::string> Factory::getNames(const bool ignore_aliases, const bool ignore_internals)
+    {
+        auto const& object_classes = getClasses();
+        std::vector<std::string> names;
+        names.reserve(object_classes.size());
+        
+        for(const auto& object_class : object_classes)
+        {
+            if(!object_class->isInternal() || !ignore_internals)
+            {
+                names.emplace_back(object_class->getClassName());
+                
+                if(!ignore_aliases && object_class->hasAlias())
+                {
+                    const auto aliases = object_class->getAliases();
+                    names.insert(names.end(), aliases.begin(), aliases.end());
+                }
+            }
+        }
+        
+        return names;
+    }
+    
+    std::string Factory::sanitizeName(std::string const& name)
+    {
+        std::string model_name = "cicm.kiwi.object.";
+        
+        static const std::string valid_chars("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890_.");
+        
+        for(auto const& c : name)
+        {
+            if(c == '~')
+            {
+                model_name.append("_tilde");
+                continue;
+            }
+            
+            model_name += (valid_chars.find(c) != std::string::npos) ? c : '_';
+        }
+        
+        return model_name;
+    }
+    
+    auto Factory::getClasses() -> object_classes_t&
+    {
+        static object_classes_t static_object_classes;
+        return static_object_classes;
+    }
+    
+    // ================================================================================ //
+    //                             FACTORY OBJECT CLASS BASE                            //
+    // ================================================================================ //
+    
+    Factory::ObjectClassBase::ObjectClassBase(std::string const& name,
+                                              std::string const& model_name,
+                                              const ctor_fn_t ctor,
+                                              const mold_maker_fn_t mold_maker,
+                                              const mold_caster_fn_t  mold_caster)
+    : m_name(name)
+    , m_model_name(model_name)
+    , m_ctor(ctor)
+    , m_mold_maker(mold_maker)
+    , m_mold_caster(mold_caster)
+    {
+        ;
+    }
+    
+    std::string const& Factory::ObjectClassBase::getClassName() const
+    {
+        return m_name;
+    }
+    
+    std::string const& Factory::ObjectClassBase::getDataModelName() const
+    {
+        return m_model_name;
+    }
+    
+    bool Factory::ObjectClassBase::isInternal() const noexcept
+    {
+        return m_internal;
+    }
+    
+    void Factory::ObjectClassBase::setInternal(const bool is_internal) noexcept
+    {
+        m_internal = is_internal;
+    }
+    
+    bool Factory::ObjectClassBase::hasAlias() const noexcept
+    {
+        return !m_aliases.empty();
+    }
+    
+    std::set<std::string> const& Factory::ObjectClassBase::getAliases() const noexcept
+    {
+        return m_aliases;
+    }
+    
+    bool Factory::ObjectClassBase::hasAlias(std::string const& alias) const noexcept
+    {
+        return (m_aliases.count(alias) != 0);
+    }
+    
+    void Factory::ObjectClassBase::addAlias(std::string alias)
+    {
+        if(!Factory::has(alias))
+        {
+            m_aliases.emplace(std::move(alias));
+        }
+    }
+    
+    std::unique_ptr<model::Object> Factory::ObjectClassBase::create(std::vector<Atom> const& args) const
+    {
+        return m_ctor(args);
+    }
+    
+    void Factory::ObjectClassBase::moldMake(model::Object const& object, flip::Mold& mold) const
+    {
+        m_mold_maker(object, mold);
+    }
+    
+    std::unique_ptr<model::Object> Factory::ObjectClassBase::moldCast(flip::Mold const& mold) const
+    {
+        return m_mold_caster(mold);
+    }
+}}
---
+++ Modules/KiwiModel/KiwiModel_Factory.hpp
@@ -0,0 +1,130 @@
+/*
+ ==============================================================================
+ 
+ This file is part of the KIWI library.
+ - Copyright (c) 2014-2016, Pierre Guillot & Eliott Paris.
+ - Copyright (c) 2016-2017, CICM, ANR MUSICOLL, Eliott Paris, Pierre Guillot, Jean Millot.
+ 
+ Permission is granted to use this software under the terms of the GPL v3
+ (or any later version). Details can be found at: www.gnu.org/licenses
+ 
+ KIWI is distributed in the hope that it will be useful, but WITHOUT ANY
+ WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ 
+ ------------------------------------------------------------------------------
+ 
+ Contact : cicm.mshparisnord@gmail.com
+ 
+ ==============================================================================
+ */
+
+#pragma once
+
+namespace kiwi { namespace model {
+    
+    // ================================================================================ //
+    //                                   OBJECT FACTORY                                 //
+    // ================================================================================ //
+    
+    template<class TModel>
+    struct Factory::isValidObject
+    {
+        enum
+        {
+            value = std::is_base_of<model::Object, TModel>::value
+            && !std::is_abstract<TModel>::value
+            && std::is_constructible<TModel, std::string, std::vector<Atom>>::value
+            && std::is_constructible<TModel, flip::Default &>::value
+        };
+    };
+    
+    template<class TInheritedObject>
+    struct Factory::isValidInheritableObject
+    {
+        enum
+        {
+            value = (std::is_same<model::Object, TInheritedObject>::value
+                     || std::is_base_of<model::Object, TInheritedObject>::value)
+        };
+    };
+    
+    template<class TModel, class TInheritedObject>
+    Factory::ObjectClass<TModel, TInheritedObject>& Factory::add(std::string const& name)
+    {
+        static_assert(isValidInheritableObject<TInheritedObject>::value, "Not a valid inheritable Object");
+        static_assert(isValidObject<TModel>::value, "Not a valid Object");
+        
+        assert(DataModel::has<TInheritedObject>() && "Inherited object must be declared first");
+        
+        assert((!name.empty() && !DataModel::has<TModel>())
+               && "Object name empty or object class already added");
+        
+        // check if the name match the name of another object in the factory.
+        if(has(name))
+        {
+            throw std::runtime_error("The \"" + name + "\" object is already in the factory");
+        }
+        
+        auto& object_classes = getClasses();
+        const auto it = object_classes.emplace(object_classes.end(),
+                                               std::make_unique<ObjectClass<TModel, TInheritedObject>>(name));
+        
+        return dynamic_cast<ObjectClass<TModel, TInheritedObject>&>(*(it->get()));
+    }
+    
+    template<class TModel>
+    auto Factory::getCtor(std::string const& name) -> ctor_fn_t
+    {
+        return [name](std::vector<Atom> const& args)
+        {
+            return std::make_unique<TModel>(name, args);
+        };
+    }
+    
+    template<class TModel>
+    auto Factory::getMoldMaker() -> mold_maker_fn_t
+    {
+        return [](model::Object const& object, flip::Mold& mold)
+        {
+            // make a mold with container_flag active
+            mold.make(static_cast<TModel const&>(object), false);
+        };
+    }
+    
+    template<class TModel>
+    auto Factory::getMoldCaster() -> mold_caster_fn_t
+    {
+        return [](flip::Mold const& mold)
+        {
+            flip::Default d;
+            auto object_uptr = std::make_unique<TModel>(d);
+            mold.cast<TModel>(static_cast<TModel&>(*(object_uptr.get())));
+            return object_uptr;
+        };
+    }
+    
+    // ================================================================================ //
+    //                               FACTORY OBJECT CLASS                               //
+    // ================================================================================ //
+    
+    template<class TObjectClass, class TInheritedObject>
+    Factory::ObjectClass<TObjectClass, TInheritedObject>::ObjectClass(std::string const& name)
+    : Factory::ObjectClassBase(name,
+                               Factory::sanitizeName(name),
+                               getCtor<class_t>(name),
+                               getMoldMaker<class_t>(),
+                               getMoldCaster<class_t>())
+    , m_flip_class(DataModel::declare<class_t>())
+    {
+        m_flip_class.name(getDataModelName().c_str())
+        .template inherit<TInheritedObject>();
+    }
+    
+    template<class TObjectClass, class TInheritedObject>
+    template<class U, U TObjectClass::*ptr_to_member>
+    void Factory::ObjectClass<TObjectClass, TInheritedObject>::addMember(char const* name)
+    {
+        m_flip_class.template member(name);
+    }
+}}
